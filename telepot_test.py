6bdc104bac360c0dbf0a84469bcb2f38f271132a
hanshenrikjeppesen <info@frist.dk>
2017-10-18 14:25:12 +0200

    Update telepot_test.py

--- telepot_test.py
+++ telepot_test.py
@@ -16,10 +16,10 @@ def off(pin):
         return
 
 # to use Raspberry Pi board pin numbers
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # set up GPIO output channel
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -28,9 +28,9 @@ def handle(msg):
     print ('Got command: {}'.format(command))
 
     if command == 'on':
-       bot.sendMessage(chat_id, on(11))
+       bot.sendMessage(chat_id, on(17))
     elif command =='off':
-       bot.sendMessage(chat_id, off(11))
+       bot.sendMessage(chat_id, off(17))
 
 bot = telepot.Bot('479647911:AAF3lzp-5g9G-VBkU31duAMwWsTVHnmUzBA')
 bot.message_loop(handle)
