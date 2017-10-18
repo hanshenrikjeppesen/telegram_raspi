931f49d3aa853145de8abd456e826beddc2b1503
hanshenrikjeppesen <info@frist.dk>
2017-10-18 22:07:59 +0200

    change in incoming text

--- telepot_test.py
+++ telepot_test.py
@@ -18,9 +18,10 @@ def off(pin):
 # to use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+PIN = 23
 
 # set up GPIO output channel
-GPIO.setup(14, GPIO.OUT)
+GPIO.setup(PIN, GPIO.OUT)
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -28,19 +29,13 @@ def handle(msg):
 
     print ('Got command: {}'.format(command))
     return command
-    if command == 'on':
-        GPIO.output(14, GPIO.HIGH)
-    if command =='off':
-        GPIO.output(14, GPIO.LOW)
+    if command == 'On':
+        GPIO.output(PIN, GPIO.HIGH)
+    if command =='Off':
+        GPIO.output(PIN, GPIO.LOW)
 
 bot = telepot.Bot('479647911:AAF3lzp-5g9G-VBkU31duAMwWsTVHnmUzBA')
 bot.message_loop(handle)
 print ('I am listening...')
-if command == 'on':
-    print('YES')
-    GPIO.output(14, GPIO.HIGH)
-if command =='off':
-    GPIO.output(14, GPIO.LOW)
-
 while 1:
      time.sleep(10)
