afe7ce9c60e560187d3b71c9672382a3a9a171ee
hanshenrikjeppesen <info@frist.dk>
2017-10-19 09:09:51 +0200

    Update soundSensorTest.py

--- soundSensorTest.py
+++ soundSensorTest.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import RPi.GPIO as GPIO
+
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+PIN = 14
+
+# set up GPIO output channel
+GPIO.setup(PIN, GPIO.IN)
+
+def measure_sound():
+  
