bec418ac1afcceca49cc586cbfcc20bd986d957d
hanshenrikjeppesen <info@frist.dk>
2017-10-19 09:19:15 +0200

    new code from work-pc

--- soundSensorTest.py
+++ soundSensorTest.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges. You can achieve this by using 'sudo' to run your script")
+import time
 
 # to use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 PIN = 14
 
-# set up GPIO output channel
+# set up GPIO input channel
 GPIO.setup(PIN, GPIO.IN)
 
 def measure_sound():
-  
+    sound = GPIO.input(14)
+    return sound
+
+while True:
+    sound = measure_sound()
+    print(sound)
+    time.sleep(1)
